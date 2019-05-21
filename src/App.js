5d9869d871c94b476525dbd5a54c21a645abec2d
PCMACHERO <pcmacmen@gmail.com>
2019-05-21 14:23:47 -0700

    fixed img link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom'
 import SignUpPage from './signuppage';
 import LandingPage from './landingpage/landingpage';
+import S1HomePage from './site1/s1homepage';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <React.Fragment>
         <Route path="/signup" component={SignUpPage} exact/>
         <Route path="/landingpage" component={LandingPage} exact/>
+        <Route path="/site1" component={S1HomePage} exact/>
 
       </React.Fragment>
         
---
+++ src/site1/s1homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './site1.css'
+
+export default class S1HomePage extends React.Component {
+    render(){
+        return (
+            <div>Hello Site</div>
+        )
+    }
+}
